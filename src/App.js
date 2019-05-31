a0c9907bc9dd06a747f1c93eab8d037ed1873a49
Lena Ryan <alissadyiphil@gmail.com>
2019-05-31 10:51:11 +0300

    Функция для вычисления победителя

--- src/App.js
+++ src/App.js
@@ -10,6 +10,44 @@ class App extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
+  searchWinner() {
+    let winCombos = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7]
+    ];
+
+    let cells = document.querySelectorAll('.cell');
+
+    let orderValue = [];
+
+    cells.forEach(cell => {
+      orderValue[Number.parseInt(cell.getAttribute('order'))] = cell.innerText;
+    })
+
+    /*
+      Для каждой комбинации:
+      если в ячейке с таким ордером
+      значение равно всем значениям
+      в других ячейках этой комбинации,
+      то выиграл игрок, ставивший знак,
+      указанный в значении
+    */
+
+    winCombos.forEach(combo => {
+      if (orderValue[combo[0]] !== '' && orderValue[combo[0]] === orderValue[combo[1]] && orderValue[combo[0]] === orderValue[combo[2]]) {
+        console.log(orderValue[combo[0]] + ' wins');
+        cells.forEach(cell => cell.setAttribute('disabled', 'disabled'));
+      }
+    })
+
+  }
+
   handleClick(e) {
     let moveNumber = this.state.moveNum;
     
@@ -21,12 +59,14 @@ class App extends React.Component {
         e.target.innerText = 'O';
       }
 
+      if (moveNumber > 3 && moveNumber < 9) {
+        this.searchWinner();
+      }
+
       this.setState({
         moveNum: ++moveNumber
       });
     }
-
-    console.log(e.target.getAttribute('order'));
   }
 
   render() {
